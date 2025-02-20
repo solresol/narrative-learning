f5655eabdca7d0edccb1593bbca7d97d15100484
Greg Baker <gregb@ifost.org.au>
2025-02-20 23:15:46 +1100

    Be more verbose, and also switch to using uv to run python scripts.

--- loop.sh
+++ loop.sh
@@ -1,23 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ "$ROUND_TRACKING_FILE" = "" ]
 then
     ROUND_TRACKING_FILE=.round-tracking-file
 fi
 
+echo "Using $ROUND_TRACKING_FILE to track the current round"
+
 if [ ! -e $ROUND_TRACKING_FILE ]
 then
     ROUND=1
+    echo "Using a default of 1"
 else
     ROUND=$(< $ROUND_TRACKING_FILE)
 fi
 
+echo "Starting at round $ROUND"
+
 while true
 do
-    ./process_round.py --round $ROUND
-    ./train.py --round-id $ROUND --round-tracking-file $ROUND_TRACKING_FILE
+    uv run process_round.py --round $ROUND --loop
+    uv run train.py --round-id $ROUND --round-tracking-file $ROUND_TRACKING_FILE
     ROUND=$(< $ROUND_TRACKING_FILE)
-    if ./report-script.py --metric accuracy --validation --patience 3 
+    if uv run report-script.py --metric accuracy --validation --patience 3 
     then
 	# Successful. Worth continuing
 	continue
