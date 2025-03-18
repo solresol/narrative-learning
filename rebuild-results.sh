8fadc48af92e03193abb8df44f900f7f3323cf34
Greg Baker <gregb@ifost.org.au>
2025-03-18 19:05:21 +1100

    We might as well put the results into git ASAP.

--- rebuild-results.sh
+++ rebuild-results.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 rm -f outputs/wisconsin_results.csv outputs/titanic_results.csv outputs/southgermancredit_results.csv \
-    && make outputs/wisconsin_results.csv outputs/titanic_results.csv outputs/southgermancredit_results.csv 
+    && make outputs/wisconsin_results.csv outputs/titanic_results.csv outputs/southgermancredit_results.csv \
+    && git add outputs/wisconsin_results.csv outputs/titanic_results.csv outputs/southgermancredit_results.csv \
+    && git commit -m"Latest experimental results"
