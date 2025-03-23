b19eb39c50060207c147dd4c41bd43bdedd1bdbc
Greg Baker <gregb@ifost.org.au>
2025-03-23 17:56:34 +1100

    Automate the building of more results.

--- rebuild-results.sh
+++ rebuild-results.sh
@@ -3,4 +3,6 @@
 rm -f outputs/wisconsin_results.csv outputs/titanic_results.csv outputs/southgermancredit_results.csv \
     && make outputs/wisconsin_results.csv outputs/titanic_results.csv outputs/southgermancredit_results.csv \
     && git add outputs/wisconsin_results.csv outputs/titanic_results.csv outputs/southgermancredit_results.csv \
+    && make outputs/impact-of-samples.tex outputs/model_details.tex outputs/titanic_by_model_size.png outputs/wisconsin_by_model_size.png outputs/southgermancredit_by_model_size.png \
+    && git add -f outputs/impact-of-samples.tex outputs/sample-count-impact-chart.png outputs/model_details.tex outputs/titanic_by_model_size.png outputs/wisconsin_by_model_size.png outputs/southgermancredit_by_model_size.png \
     && git commit -m"Latest experimental results"
