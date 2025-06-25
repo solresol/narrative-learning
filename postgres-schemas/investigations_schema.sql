93c62d3b65aabc36f4d880fcb11c9c940559aada
Greg Baker <gregb@ifost.org.au>
2025-06-25 17:53:49 +1000

    Not null, not default round_number... changes on the investigation table.

--- postgres-schemas/investigations_schema.sql
+++ postgres-schemas/investigations_schema.sql
@@ -14,8 +14,8 @@ CREATE TABLE models (
 
 CREATE TABLE investigations (
     id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    dataset TEXT REFERENCES datasets(dataset),
-    model TEXT REFERENCES models(model),
+    dataset TEXT not null REFERENCES datasets(dataset),
+    model TEXT not null REFERENCES models(model),
     sqlite_database TEXT,
     round_tracking_file TEXT,
     dump_file TEXT,
