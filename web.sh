6800edd93aaa86d4e55dbbe81d2c04a1be30e38b
Greg Baker <gregb@ifost.org.au>
2025-07-14 13:53:28 +1000

    Show progress

--- web.sh
+++ web.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-uv run export_website.py && \
+uv run export_website.py --progress && \
 rsync -av website/ merah.cassia.ifost.org.au:/var/www/vhosts/narrative-learning.symmachus.org/htdocs/
