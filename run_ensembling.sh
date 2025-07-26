04be6163b565c02d0e92dad24da35c827a720ea1
Greg Baker <gregb@ifost.org.au>
2025-07-26 18:07:23 +1000

    Newlines are nice.

--- run_ensembling.sh
+++ run_ensembling.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-uv run results_ensembling.py --progress titanic && uv run results_ensembling.py --progress wisconsin && uv run results_ensembling.py --progress southgermancredit && uv run results_ensembling.py --progress -no-decodex espionage && uv run results_ensembling.py --progress --no-decodex timetravel_insurance && uv run results_ensembling.py --progress --no-decodex potions
+uv run results_ensembling.py --progress titanic && \
+uv run results_ensembling.py --progress wisconsin && \
+uv run results_ensembling.py --progress southgermancredit && \
+uv run results_ensembling.py --progress --no-decodex espionage && \
+uv run results_ensembling.py --progress --no-decodex timetravel_insurance && \
+uv run results_ensembling.py --progress --no-decodex potions
